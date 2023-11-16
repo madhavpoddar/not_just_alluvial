00ba2a301e8be6f21ebd7a5f7eefb9ba17564273
Jan-Tobias Sohns <j_sohns12@cs.uni-kl.de>
2023-11-16 14:49:56 +0100

    update vispub dataset

--- hd_cluster_analysis.py
+++ hd_cluster_analysis.py
@@ -38,7 +38,8 @@ drcl_vis = drcl_vis_wrapper(
 ### Dataset of authors regarding collaborations in publications on Visualization (VIS & predecessors) from 1990-2022 grouped by 5 year intervals.
 ### Dataset is filtered from 6600 authors to 36 by only including 
 ### authors that collaborated in at least 3 of the time intervals (->102) with at least 2 other authors (->36).
-# df = read_csv_file("vispub_filtered.csv")
+# df = read_csv_file("vispub_filtered_36.csv")
+# df = read_csv_file("vispub_filtered_102.csv")
 # col_names = ['90-95', '95-00', '00-05', '05-10', '10-15', '15-20', '20-22']
 # drcl_vis = drcl_vis_wrapper(df, col_names)
 
